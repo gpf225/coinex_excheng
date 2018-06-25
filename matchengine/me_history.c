8d695c0cf6b6e90b00b6c747a0c29cd75ad479b5
Haipo Yang <yang@haipo.me>
2018-06-25 15:21:41 +0800

    fix bug

--- matchengine/me_history.c
+++ matchengine/me_history.c
@@ -348,7 +348,7 @@ int append_order_deal_history(double t, uint64_t deal_id, order_t *ask, int ask_
     append_order_deal(t, bid->user_id, deal_id, bid->id, ask->id, bid_role, price, amount, deal, bid_fee_asset, bid_fee, ask_fee_asset, ask_fee);
 
     append_user_deal(t, ask->user_id, ask->market, deal_id, ask->id, bid->id, ask->side, ask_role, price, amount, deal, ask_fee_asset, ask_fee, bid_fee_asset, bid_fee);
-    append_user_deal(t, bid->user_id, ask->market, deal_id, bid->id, ask->id, bid->side, bid_role, price, amount, deal, ask_fee_asset, bid_fee, bid_fee_asset, ask_fee);
+    append_user_deal(t, bid->user_id, ask->market, deal_id, bid->id, ask->id, bid->side, bid_role, price, amount, deal, bid_fee_asset, bid_fee, ask_fee_asset, ask_fee);
 
     return 0;
 }
