895e4e5fe4e66887f02518705a7e945d2d52393e
vsgoster <vsgoster@126.com>
2018-08-15 22:35:32 +0800

    Adust deployment document

--- deploy/20180815/create_table.sql
+++ deploy/20180815/create_table.sql
@@ -1,3 +1,21 @@
+CREATE TABLE `slice_stop_example` (
+    `id`            BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    `t`             TINYINT UNSIGNED NOT NULL,
+    `side`          TINYINT UNSIGNED NOT NULL,
+    `create_time`   DOUBLE NOT NULL,
+    `update_time`   DOUBLE NOT NULL,
+    `user_id`       INT UNSIGNED NOT NULL,
+    `market`        VARCHAR(30) NOT NULL,
+    `source`        VARCHAR(30) NOT NULL,
+    `fee_asset`     VARCHAR(30) NOT NULL,
+    `fee_discount`  DECIMAL(40,4) NOT NULL,
+    `stop_price`    DECIMAL(40,8) NOT NULL,
+    `price`         DECIMAL(40,8) NOT NULL,
+    `amount`        DECIMAL(40,8) NOT NULL,
+    `taker_fee`     DECIMAL(40,4) NOT NULL,
+    `maker_fee`     DECIMAL(40,4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `stop_history_example` (
     `id`            BIGINT UNSIGNED NOT NULL PRIMARY KEY,
     `create_time`   DOUBLE NOT NULL,
