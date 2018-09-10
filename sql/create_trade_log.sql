062885dfb2d0367418e372cb4a011c50114bbfd1
vsgoster <vsgoster@126.com>
2018-09-10 12:41:16 +0800

    recover

--- sql/create_trade_log.sql
+++ sql/create_trade_log.sql
@@ -26,14 +26,14 @@ CREATE TABLE `slice_order_example` (
     `source`        VARCHAR(30) NOT NULL,
     `fee_asset`     VARCHAR(30) NOT NULL,
     `fee_discount`  DECIMAL(40,4) NOT NULL,
-    `price`         DECIMAL(40,12) NOT NULL,
+    `price`         DECIMAL(40,8) NOT NULL,
     `amount`        DECIMAL(40,8) NOT NULL,
     `taker_fee`     DECIMAL(40,4) NOT NULL,
     `maker_fee`     DECIMAL(40,4) NOT NULL,
     `left`          DECIMAL(40,8) NOT NULL,
     `frozen`        DECIMAL(40,8) NOT NULL,
     `deal_stock`    DECIMAL(40,8) NOT NULL,
-    `deal_money`    DECIMAL(40,20) NOT NULL,
+    `deal_money`    DECIMAL(40,16) NOT NULL,
     `deal_fee`      DECIMAL(40,20) NOT NULL,
     `asset_fee`     DECIMAL(40,20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -49,8 +49,8 @@ CREATE TABLE `slice_stop_example` (
     `source`        VARCHAR(30) NOT NULL,
     `fee_asset`     VARCHAR(30) NOT NULL,
     `fee_discount`  DECIMAL(40,4) NOT NULL,
-    `stop_price`    DECIMAL(40,12) NOT NULL,
-    `price`         DECIMAL(40,12) NOT NULL,
+    `stop_price`    DECIMAL(40,8) NOT NULL,
+    `price`         DECIMAL(40,8) NOT NULL,
     `amount`        DECIMAL(40,8) NOT NULL,
     `taker_fee`     DECIMAL(40,4) NOT NULL,
     `maker_fee`     DECIMAL(40,4) NOT NULL
