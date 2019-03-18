bfccca047a0d84f3033b2006372c6f199ac0f426
vsgoster <vsgoster@126.com>
2019-03-18 14:38:12 +0800

    add deploy documentation for matchengine

---
+++ build_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ./network/ &&  make clean && make
+cd ../utils/ &&  make clean && make
+cd ../accessrest/ &&  make clean && make
+cd ../accesshttp/ &&  make clean && make
+cd ../accessws/ &&  make clean && make
+cd ../marketprice/ &&  make clean && make
+cd ../matchengine/ &&  make clean && make
+cd ../readhistory/ &&  make clean && make
+cd ../cache/ &&  make clean && make
+cd ../longpoll/ &&  make clean && make
+cd ../monitoragent/ &&  make clean && make
+cd ../monitorcenter/ &&  make clean && make
