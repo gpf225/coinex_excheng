5cf99c06c39d03e7eb3553e0023d09ffaab104d0
kingouxy <963473765@qq.com>
2019-04-16 22:30:31 +0800

    迁移sql修改

--- sql/create_trade_history.sql
+++ sql/create_trade_history.sql
@@ -8,7 +8,6 @@ CREATE TABLE `balance_history_example` (
     `change`        DECIMAL(40,20) NOT NULL COMMENT "资产变更",  # 由原来8位变成20位
     `balance`       DECIMAL(40,20) NOT NULL COMMENT "变更后余额",
     `detail`        TEXT NOT NULL COMMENT "明细信息",
-    INDEX `idx_time` (`time`),
     INDEX `idx_user_time` (`user_id`, `time`),
     INDEX `idx_user_business_time` (`user_id`, `business`, `time`),
     INDEX `idx_user_asset_business_time` (`user_id`, `asset`, `business`, `time`)
@@ -35,7 +34,7 @@ CREATE TABLE `order_history_example` (
     `deal_money`    DECIMAL(40,20) NOT NULL COMMENT "定价币种已交易数量", # 由原来16位变成20位，8+12=20，如果价格扩展到20位，那么我们就做四舍五入.
     `deal_fee`      DECIMAL(40,20) NOT NULL COMMENT "定价货币已产生的手续费",
     `asset_fee`     DECIMAL(40,20) NOT NULL COMMENT "手续费币种已产生的手续费",
-    INDEX `idx_time` (`create_time`),
+    INDEX `idx_order_id` (`order_id`),
     INDEX `idx_user_time` (`user_id`, `create_time`),
     INDEX `idx_user_side_time` (`user_id`, `side`, `create_time`),
     INDEX `idx_user_market_time` (`user_id`, `market`, `create_time`),
@@ -61,7 +60,6 @@ CREATE TABLE `stop_history_example` (
     `taker_fee`     DECIMAL(40,4) NOT NULL COMMENT "taker费率",
     `maker_fee`     DECIMAL(40,4) NOT NULL COMMENT "maker费率",
     `status`        TINYINT UNSIGNED NOT NULL COMMENT "计划委托单状态，成功，失败",
-    INDEX `idx_time` (`create_time`),
     INDEX `idx_user_time` (`user_id`, `create_time`),
     INDEX `idx_user_side_time` (`user_id`, `side`, `create_time`),
     INDEX `idx_user_market_time` (`user_id`, `market`, `create_time`),
@@ -87,7 +85,7 @@ CREATE TABLE `user_deal_history_example` (
     `deal_fee`      DECIMAL(40,20) NOT NULL COMMENT "对手单交易手续费",
     `fee_asset`      VARCHAR(30) NOT NULL COMMENT "本交易手续费货币类型",
     `deal_fee_asset` VARCHAR(30) NOT NULL COMMENT "对手交易手续费货币类型",
-    INDEX `idx_time` (`time`),
+    INDEX `idx_order_id_deal_id` (`order_id`, `deal_id`),
     INDEX `idx_user_time` (`user_id`, `time`),
     INDEX `idx_user_side_time` (`user_id`, `side`, `time`),
     INDEX `idx_user_market_time` (`user_id`, `market`, `time`),
