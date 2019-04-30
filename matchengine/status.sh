499fba3458c900baef47fb1a03bf55aa03b0dbea
yangxiaoqiang2018 <837688516@qq.com>
2019-04-30 17:15:06 +0800

    test api

---
+++ matchengine/status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "status"|nc 127.0.0.1 8316
+echo "status"|nc 127.0.0.1 8317
+echo "status"|nc 127.0.0.1 8318
+echo "status"|nc 127.0.0.1 8319
+echo "status"|nc 127.0.0.1 8320
+echo "status"|nc 127.0.0.1 8321
