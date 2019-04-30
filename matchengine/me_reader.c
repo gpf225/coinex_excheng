808b2c9a767b82a64153b93b46394d007d02c088
yangxiaoqiang2018 <837688516@qq.com>
2019-04-30 22:09:10 +0800

    reader unfreeze

--- matchengine/me_reader.c
+++ matchengine/me_reader.c
@@ -1002,6 +1002,47 @@ static sds queue_status(sds reply)
     return reply;
 }
 
+static sds on_cmd_unfreeze(const char *cmd, int argc, sds *argv)
+{
+    if (argc != 3) {
+        sds reply = sdsempty();
+        return sdscatprintf(reply, "usage: %s user_id asset amount\n", cmd);
+    }
+
+    uint32_t user_id = strtoul(argv[0], NULL, 0);
+    if (user_id <= 0) {
+        return sdsnew("failed, user_id error\n");
+    }
+
+    char *asset = strdup(argv[1]);
+    if (!asset) {
+        return sdsnew("failed, asset error\n");
+    }
+    if (!asset_exist(asset)) {
+        free(asset);
+        return sdsnew("failed, asset not exist\n");
+    }
+
+    mpd_t *amount = decimal(argv[2], asset_prec(asset));
+    if (!amount) {
+        free(asset);
+        return sdsnew("failed, amount error\n");
+    }
+
+    mpd_t *frozen = balance_unfreeze(user_id, BALANCE_TYPE_FROZEN, asset, amount);
+    if (!frozen) {
+        free(asset);
+        mpd_del(amount);
+        sds reply = sdsempty();
+        return sdscatprintf(reply, "unfreeze failed, user_id: %d\n", user_id);
+    }
+
+    free(asset);
+    mpd_del(amount);
+    sds reply = sdsempty();
+    return sdscatprintf(reply, "unfreeze success, user_id: %d\n", user_id);
+}
+
 static sds on_cmd_status(const char *cmd, int argc, sds *argv)
 {
     sds reply = sdsempty();
@@ -1024,6 +1065,7 @@ static int init_cli()
     }
 
     cli_svr_add_cmd(svrcli, "status", on_cmd_status);
+    cli_svr_add_cmd(svrcli, "unfreeze", on_cmd_unfreeze);
     return 0;
 }
 
