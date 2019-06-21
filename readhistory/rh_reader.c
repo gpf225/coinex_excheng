f8fbc9321f2783ffe61557a45bf355d03d46244d
zuoweiliu <zuoweiliu@126.com>
2019-06-21 11:43:44 +0800

    线上order by不使用索引问题

--- readhistory/rh_reader.c
+++ readhistory/rh_reader.c
@@ -110,7 +110,7 @@ json_t *get_user_order_history(MYSQL *conn, uint32_t user_id, int32_t account,
         sql = sdscatprintf(sql, " AND `create_time` < %"PRIu64, end_time);
     }
 
-    sql = sdscatprintf(sql, " ORDER BY `order_id` DESC");
+    sql = sdscatprintf(sql, " ORDER BY `create_time` DESC");
     if (offset) {
         sql = sdscatprintf(sql, " LIMIT %zu, %zu", offset, limit);
     } else {
