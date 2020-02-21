6a0a3e38b8b179d58d5e4f2020f880d5e161cc8a
yangxiaoqiang2018 <837688516@qq.com>
2020-02-21 22:12:06 +0800

    删除ws优化代码

--- accessws/aw_deals.h
+++ accessws/aw_deals.h
@@ -2,8 +2,6 @@
 # define _AW_DEALS_H_
 
 int init_deals(void);
-int deals_subscribe_user(nw_ses *ses, uint32_t user_id, const char *market);
-int deals_unsubscribe_user(nw_ses *ses, uint32_t user_id, const char *market);
 int deals_subscribe(nw_ses *ses, const char *market, uint32_t user_id);
 int deals_unsubscribe(nw_ses *ses, uint32_t user_id);
 int deals_new(uint32_t user_id, uint64_t id, double timestamp, int type, const char *market, const char *amount, const char *price);
