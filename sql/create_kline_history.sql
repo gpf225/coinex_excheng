10ea27d6739ea5b5edb14fa161acbcc543407320
root <963473765@qq.com>
2020-04-28 03:47:26 +0000

    marketprice: 写历史数据库字段修改

--- sql/create_kline_history.sql
+++ sql/create_kline_history.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `kline_history_example` (
     `id`            BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `market`        VARCHAR(64) NOT NULL,
-    `class`         TINYINT UNSIGNED NOT NULL,
+    `t`             TINYINT UNSIGNED NOT NULL,
     `timestamp`     BIGINT UNSIGNED NOT NULL,
     `open`          DECIMAL(40,20) NOT NULL,
     `close`         DECIMAL(40,20) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `kline_history_example` (
     `low`           DECIMAL(40,20) NOT NULL,
     `volume`        DECIMAL(40,20) NOT NULL,
     `deal`          DECIMAL(40,20) NOT NULL,
-    INDEX `idx_market_class` (`market`, `class`),
-    INDEX `idx_market_class_timestamp` (`market`, `class`, `timestamp`),
-    UNIQUE INDEX `uidx_market_class_timestamp` (`market`, `class`, `timestamp`)
+    INDEX `idx_market_type` (`market`, `t`),
+    INDEX `idx_market_type_timestamp` (`market`, `t`, `timestamp`),
+    UNIQUE INDEX `uidx_market_type_timestamp` (`market`, `t`, `timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
