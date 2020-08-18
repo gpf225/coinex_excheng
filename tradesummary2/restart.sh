4f890a5c6d2f8c9fc0c5e3f483f970a9e9ce63e1
yangxiaoqiang2018 <837688516@qq.com>
2020-08-18 11:48:57 +0800

    恢复原样

--- tradesummary2/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-killall -s SIGQUIT tradesummary.exe
-sleep 1
-./tradesummary.exe config.json
--- tradesummary2/ts_server.h
+++
@@ -1,7 +0,0 @@
-# ifndef _TS_SERVER_H_
-# define _TS_SERVER_H_
-
-int init_server(void);
-
-# endif
-
